71d7f02ee38cb24cf20a1ca5fbffec9a9c4a7e48
ksaifahmed <ksaifahmed@gmail.com>
2022-06-01 21:56:12 +0600

    offline 1 init

---
+++ graphics_offline_1/vect.h
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+struct point
+{
+	double x,y,z;
+};
+
+point move_along_vect(point p, point vect, double units)
+{
+    return p;
+}
+
+
