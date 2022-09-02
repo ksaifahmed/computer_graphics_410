a98625f8467cef9de9af90a12ed46b98526bbfa3
ksaifahmed <ksaifahmed@gmail.com>
2022-09-02 21:01:34 +0600

    unweighted area sampling init

--- scan converting lines/1705110_main.cpp
+++ scan converting lines/1705110_main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 
-void captureMidPoint() {
+void capture_MidPoint() {
 	bitmap_image image(W, H);
 	for (int i = 0; i < W; i++)
 		for (int j = 0; j < H; j++)
@@ -16,8 +16,21 @@ void captureMidPoint() {
     image.save_image("1_R.bmp");
 }
 
+void capture_UnweightedAreaSampling() {
+    bitmap_image image(W, H);
+    for (int i = 0; i < W; i++)
+        for (int j = 0; j < H; j++)
+            image.set_pixel(i, j, 0, 0, 0);
 
-void captureWeightedAreaSampling() {
+    for (auto & line : lines){
+        scanLine_UnweightedAreaSamplingAntiAliased(line, image);
+    }
+    cout << "2_RUA Image is captured" << endl;
+    image.save_image("2_RUA.bmp");
+}
+
+
+void capture_WeightedAreaSampling() {
 	bitmap_image image(W, H);
 	for (int i = 0; i < W; i++)
 		for (int j = 0; j < H; j++)
@@ -50,8 +63,9 @@ int main()
     for (auto & line : lines)
         cout << line;
 
-    captureMidPoint();
-    captureWeightedAreaSampling();
+    capture_MidPoint();
+    capture_UnweightedAreaSampling();      
+    capture_WeightedAreaSampling();
 
     fin.close();
     return 0;
--- scan converting lines/1705110_scanlines.hpp
+++ scan converting lines/1705110_scanlines.hpp
@@ -6,6 +6,8 @@
 #include "1705110_line.hpp"
 using namespace std;
 
+
+
 // Midpoint Line Algorithm for all slopes ==================================================================
 void MidpointLine(Line line, bitmap_image &image, bool isLargeGradient, bool isGradientNegative, Color color)
 {
@@ -59,12 +61,13 @@ void scanLine_MidPoint(Line line, bitmap_image &image)
 
 
 
+
 // Anti-Aliasing using Weighted Area Sampling =======================================================
 double Filter(int distance){
     return max(0.0, 1-0.25*distance);
 }
 
-void IntensifyPixel(int x, int y, double distance, bitmap_image &image, Color color, bool isSwap)
+void IntensifyPixelWA(int x, int y, double distance, bitmap_image &image, Color color, bool isSwap)
 {
     double intensity = Filter(round(abs(distance)));
     if(isSwap) swap(x, y); //reflection on y=x line
@@ -73,14 +76,14 @@ void IntensifyPixel(int x, int y, double distance, bitmap_image &image, Color co
     image.set_pixel(x, H-y, color.r*intensity, color.g*intensity, color.b*intensity);
 }
 
-void colorImageGupta(bitmap_image &image, int x, int y, Color color, bool isSwap, int two_v_dx, double invDenom, double two_dx_invDenom)
+void colorImageWA(bitmap_image &image, int x, int y, Color color, bool isSwap, int two_v_dx, double invDenom, double two_dx_invDenom)
 {
     // for SMOOTH antialiasing
-    IntensifyPixel(x, y, two_v_dx * invDenom, image, color, isSwap);
-    IntensifyPixel(x, y + 1, two_dx_invDenom - two_v_dx * invDenom, image, color, isSwap);
-    IntensifyPixel(x, y - 1, two_dx_invDenom + two_v_dx * invDenom, image, color, isSwap);
-    IntensifyPixel(x, y + 2, 2*two_dx_invDenom - (two_v_dx * invDenom), image, color, isSwap);
-    IntensifyPixel(x, y - 2, 2*two_dx_invDenom + (two_v_dx * invDenom), image, color, isSwap);
+    IntensifyPixelWA(x, y, two_v_dx * invDenom, image, color, isSwap);
+    IntensifyPixelWA(x, y + 1, two_dx_invDenom - two_v_dx * invDenom, image, color, isSwap);
+    IntensifyPixelWA(x, y - 1, two_dx_invDenom + two_v_dx * invDenom, image, color, isSwap);
+    IntensifyPixelWA(x, y + 2, 2*two_dx_invDenom - (two_v_dx * invDenom), image, color, isSwap);
+    IntensifyPixelWA(x, y - 2, 2*two_dx_invDenom + (two_v_dx * invDenom), image, color, isSwap);
 }
 
 void WeightedAreaSamplingAntiAliased(Line line, bitmap_image &image, bool isLargeGradient, bool isGradientNegative, Color color)
@@ -118,9 +121,9 @@ void WeightedAreaSamplingAntiAliased(Line line, bitmap_image &image, bool isLarg
 
         isGradientNegative
             ? isLargeGradient
-                ? colorImageGupta(image, x, -y, color, isLargeGradient, two_v_dx, invDenom, two_dx_invDenom)
-                : colorImageGupta(image, -x, y, color, isLargeGradient, two_v_dx, invDenom, two_dx_invDenom)
-            : colorImageGupta(image, x, y, color, isLargeGradient, two_v_dx, invDenom, two_dx_invDenom);
+                ? colorImageWA(image, x, -y, color, isLargeGradient, two_v_dx, invDenom, two_dx_invDenom)
+                : colorImageWA(image, -x, y, color, isLargeGradient, two_v_dx, invDenom, two_dx_invDenom)
+            : colorImageWA(image, x, y, color, isLargeGradient, two_v_dx, invDenom, two_dx_invDenom);
     }
 }
 
@@ -139,7 +142,76 @@ void scanLine_WeightedAreaSamplingAntiAliased(Line line, bitmap_image &image)
     //color.b = 0;
     WeightedAreaSamplingAntiAliased(line, image, abs(m) > 1, m < 0, color);
 }
-// =======================================================================================
+// ==================================================================================================
+
+
+
+
+// Anti-Aliasing using Unweighted Area Sampling =====================================================
+void IntensifyPixelUA(int x, int y, bitmap_image &image, Color color, bool isSwap)
+{
+    if(isSwap) swap(x, y); //reflection on y=x line
+    if(!isInsideBounds(x, y))
+        return;
+    image.set_pixel(x, H-y, color.r, color.g, color.b);
+}
+
+void colorImageUA(bitmap_image &image, int x, int y, Color color, bool isSwap)
+{
+    IntensifyPixelUA(x, y, image, color, isSwap);
+    IntensifyPixelUA(x, y+1, image, color, isSwap);
+    IntensifyPixelUA(x, y-1, image, color, isSwap);
+}
+
+void UnweightedAreaSamplingAntiAliasedLines(Line line, bitmap_image &image, bool isLargeGradient, bool isGradientNegative, Color color)
+{
+    if(isLargeGradient) {//reflection on y=x line
+        swap(line.x1, line.y1);
+        swap(line.x0, line.y0);
+    }
+
+    int dx = line.x1 - line.x0;
+    int dy = line.y1 - line.y0;
+    int d = 2 * dy - dx;
+    int incrE = 2 * dy;
+    int incrNE = 2 * (dy - dx);
+
+    int x = line.x0;
+    int y = line.y0;
+
+    while (x < line.x1) {
+        if (d < 0) {
+            d += incrE;
+            x++;
+        } else {
+            d += incrNE;
+            x++;
+            y++;
+        }
+
+        isGradientNegative
+            ? isLargeGradient
+                ? colorImageUA(image, x, -y, color, isLargeGradient)
+                : colorImageUA(image, -x, y, color, isLargeGradient)
+            : colorImageUA(image, x, y, color, isLargeGradient);
+    }
+}
+
+void scanLine_UnweightedAreaSamplingAntiAliased(Line line, bitmap_image &image)
+{
+    double m = (double)(line.y1 - line.y0) / (double)(line.x1 - line.x0);
+    Color color(255, 255, 255);
+    if(m < 0) {
+        line.x0 = -line.x0;
+        line.x1 = -line.x1;
+        swap(line.x0, line.x1);
+        swap(line.y0, line.y1);
+        //color.r = 0;
+        UnweightedAreaSamplingAntiAliasedLines(line, image, abs(m) > 1, m < 0, color);
+    }
+    //color.b = 0;
+    UnweightedAreaSamplingAntiAliasedLines(line, image, abs(m) > 1, m < 0, color);
+}
 
 
 
